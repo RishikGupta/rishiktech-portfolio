85396f717a421f9a31b65a68aecb8520e51351bb
Rishik Gupta <rishik.gupta70@gmail.com>
2025-05-15 16:08:33 -0400

    testing mobile responsive sideways

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,7 +2,10 @@ import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Home = () => {
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center relative bg-theme text-theme">
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative bg-theme text-theme mt-7"
+    >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 gradient-accent leading-right">
@@ -13,17 +16,23 @@ export const Home = () => {
             I'm a Full Stack Web Developer with hands-on experience building
             scalable web applications using the MERN stack, REST APIs, and
             modern UI frameworks. I craft responsive front-ends with React.js
-            and develop secure, feature-rich backends with Node.js and
-            Express. I bring both technical depth and real-world
-            problem-solving to every project.
+            and develop secure, feature-rich backends with Node.js and Express.
+            I bring both technical depth and real-world problem-solving to every
+            project.
           </p>
 
           <div className="flex justify-center space-x-4">
-            <a href="#projects" className="bg-blue-500 py-3 px-6 rounded 
-                font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+            <a
+              href="#projects"
+              className="bg-blue-500 py-3 px-6 rounded 
+                font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            >
               View Projects
             </a>
-            <a href="#contact" className="py-3 px-6 rounded font-medium button-outline">
+            <a
+              href="#contact"
+              className="py-3 px-6 rounded font-medium button-outline"
+            >
               Contact Me
             </a>
           </div>
